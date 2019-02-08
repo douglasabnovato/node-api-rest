bd430625e31e97ef8a9a1ff01160f8510a7c0720
@douglasabnovato <douglasabnovato@gmail.com>
2019-02-08 19:34:35 -0200

    NodeJS-api

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+//iniciando o app
+const app = express();
+
+//iniciando o db
+mongoose.connect(
+    'mongodb://localhost:27017/nodeapi',
+    { useNewUrlParser: true }
+);
+
+//primeira rota
+app.get('/', (req, res) => {
+    res.send('Hello Team Rocketseat !!');
+});
+
+app.listen(3001);
